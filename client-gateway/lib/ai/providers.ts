2c3aa5e042e4bf14c6b6295e6d3c3277638d0836
Lukas Eber <l.eber@toolplace.de>
2025-07-30 23:33:44 +0200

    .

--- client-gateway/lib/ai/providers.ts
+++ client-gateway/lib/ai/providers.ts
@@ -9,11 +9,11 @@ import { xai } from '@ai-sdk/xai';
 export const myProvider =
    customProvider({
       languageModels: {
-        'chat-model': xai('grok-3-mini-fast'),
+        'chat-model': xai('grok-3'),
         'chat-model-reasoning': wrapLanguageModel({
           model: xai('grok-3-mini-beta'),
           middleware: extractReasoningMiddleware({ tagName: 'think' }),
         }),
-        'title-model': xai('grok-3-mini-fast')
+        'title-model': xai('grok-3')
       }
     });
